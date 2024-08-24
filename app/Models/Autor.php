ab579a44b36db2c51aa5a36409dd6df66142b969
Gustavo Caldeira <gustavo.caldeira1@outlook.com>
2024-08-24 12:07:52 -0300

    feat: criação inicial do projeto e banco

---
+++ app/Models/Autor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Autor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'autores';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nome',
+        'cep',
+        'uf',
+        'cidade',
+        'bairro',
+        'logradouro',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
---
+++ app/Models/Livro.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Livro extends Model
+{
+    use HasFactory;
+
+    protected $table = 'livros';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'titulo',
+        'ano_publicacao',
+        'autor_id',
+        'ISBN',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
